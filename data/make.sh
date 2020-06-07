59d2e7af2acca97e790b9cbc18c38447e60a3e4b
Josep Sanz <josep.sanz@saltos.org>
2020-06-07 11:42:17 +0200

    Afegir alguns tweets d'avui + make.sh per fer-ho tot mes rapid

---
+++ data/make.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# comprobar el directori de treball, ha de ser l'arrel, data o images
+curdir=$(pwd | rev | cut -d/ -f1 | rev)
+
+if [ "$curdir" == "data" -o "$curdir" == "images" ]; then
+	cd ..
+fi
+
+if [ ! -d "data" -o ! -d "images" ]; then
+	echo "Error!!!"
+	exit
+fi
+
+# generar el index.html
+cd data
+php index.php > ../index.html
+cd ..
+
+# fer un check a les imatges
+cd images
+for i in $(php ../data/checkimg.php); do
+	php ../data/fiximages.php $i
+done
+cd ..
